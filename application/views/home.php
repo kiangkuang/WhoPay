0b52751ad1981c1e1812041b8663e47b8b61283d
yipjiajie <yipjiajie@gmail.com>
2016-01-23 17:34:38 +0800

    new Home.php!

--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,49 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
+
+<html>
 <head>
-	<meta charset="utf-8">
-	<title>WhoPay</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>WhoPay</title>
+  <meta name="description" content="A Simple, Elegant &amp; Flat Bootstrap Theme">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/dist/css/paper.min.css">
+  <link rel="stylesheet" href="assets/assets/css/style.css">
 </head>
 <body>
-hi
+  
+  <div class="container">
+    <div class="row" style="margin-top: 50px;">
+      <div class="col-lg-10 col-lg-offset-1">
+      <div class="jumbotron text-center">
+        <h1>WhoPay</h1>
+        <a href="#" class="btn btn-default">New bill</a>
+        <a href="#" class="btn btn-default">Join</a>
+      </div>
+      </div>
+    </div>
+
+  </div>
+  <br><br>
+
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 col-lg-offset-1 text-center">
+          <span>Released under MIT License</span> | 
+          <span>Built on <a href="http://getbootstrap.com/">Bootstrap</a></span> |
+          <span>Made by <a href="https://github.com/sudharti">sudharti</a></span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script type="text/javascript" src="assets/dist/js/vendor/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/dist/js/paper.min.js"></script>
+  <script type="text/javascript">
+    window.twttr=(function(d,s,id){var t,js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return}js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);return window.twttr||(t={_e:[],ready:function(f){t._e.push(f)}})}(document,"script","twitter-wjs"));
+  </script>
+  <script type="text/javascript" src="https://apis.google.com/js/platform.js" async defer></script>
 </body>
 </html>

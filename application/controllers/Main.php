20b47bf5283c7ae535c259bbfc671824901838b5
Kiang Kuang <kiangkuang@gmail.com>
2016-01-23 15:37:37 +0800

    stub functions

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,8 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+	// new or join button
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('home');
+	}
+
+	// manual create
+	public function create()
+	{
+		$this->load->view('create');
+	}
+
+	// ocr
+	public function ocr()
+	{
+		$this->load->view('ocr');
+	}
+
+	// lobby
+	public function receipt($receipt_id)
+	{
+		$this->load->view('receipt');
+	}
+
+	// result
+	public function result($receipt_id)
+	{
+		$this->load->view('result');
 	}
 }

0a1c00832dfd30b911ba58042889914000fc15d9
Oswell Chan <oswellchan@gmail.com>
2016-01-23 22:59:41 +0800

    Result creation algo

--- application/models/User_item_model.php
+++ application/models/User_item_model.php
@@ -9,4 +9,22 @@ class User_item_model extends MY_Model {
         $this->db_name = 'user_item';
     }
 
+    public function get_raw_result($receipt_id) 
+    {
+    	$this->db->select('`item`.`id`, `item`.`name`, `item`.`cost`, `user`.`id` AS `userId`, `user`.`name` AS `userName`');
+    	$this->db->from('user_item');
+    	$this->db->join('user', '`user_item`.`user_id` = `user`.`id`');
+    	$this->db->join('item', '`item`.`id` = `user_item`.`item_id`');
+    	$this->db->where('`user_item`.`receipt_id`', $receipt_id);
+    	$this->db->order_by('`item`.`id`', 'ASC');
+
+    	$query = $this->db->get();
+
+    	if ($query->num_rows() > 0) {
+            return $query->result();
+        }
+
+        return false;
+    }
+
 }

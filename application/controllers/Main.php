4c7e5a0703eac8a6a8aeedf2b832fda8b0ae83c7
Oswell Chan <oswellchan@gmail.com>
2016-01-24 11:33:16 +0800

    Half implemented ocr

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -27,7 +27,31 @@ class Main extends MY_Controller {
 	// ocr
 	public function ocr()
 	{
-		$this->load->view('ocr');
+		// $config['upload_path']          = './uploads/';
+  //       $config['allowed_types']        = 'gif|jpg|png';
+  //       $config['max_size']             = 1000;
+
+  //       $this->load->library('upload', $config);
+
+  //       if ( ! $this->upload->do_upload('file'))
+		// {
+  //           $error = array('error' => $this->upload->display_errors());
+
+  //           $this->load->view('upload_form', $error);
+  //       }
+  //       else
+  //       {
+  //           $data = array('upload_data' => $this->upload->data());
+
+  //           $this->load->view('upload_success', $data);
+  //       }
+
+		$fileName = "./uploads/receipt.jpg";
+
+        $command = "curl --form \"file=@".$fileName."\" --form \"apikey=helloworld\" --form \"language=eng\" https://api.ocr.space/Parse/Image";
+		$parsedData = (array) json_decode(shell_exec($command));
+
+		
 	}
 
 	//join
